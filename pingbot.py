0f1cc68c0fedfb12571d7a754d198d07e7c23873
EZLiang <47573436+EZLiang@users.noreply.github.com>
2020-01-15 17:44:45 -0500

    Update pingbot.py

--- pingbot.py
+++ pingbot.py
@@ -5,10 +5,13 @@ client = discord.Client()
 
 @client.event
 async def on_message(msg):
-    if msg.content == client.user:
+    if msg.author == client.user:
         return
     elif client.user in msg.mentions:
-        await client.send_message(msg.channel, "_I have been pinged by {}!_".format(msg.author.mention))
+        await msg.channel.send("_I have been pinged by {}!_".format(msg.author.mention))
 
 def main():
     client.run(sys.argv[1])
+
+if __name__ == "__main__":
+    main()
